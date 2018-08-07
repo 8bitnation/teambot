990454883cbf508a8f33c014c70243e274d98c80
Scott Deakin <sdeaks@gmail.com>
2018-08-07 06:45:33 +0100

    further refactor of events test

--- test/events.js
+++ test/events.js
@@ -13,9 +13,9 @@ const { HTTP_OK } = require('../app/util/const')
 describe('events', function() {
 
     // helper function
-    async function createUserAndSocket(id) {
-        await db.createUser({ id: 'u'+id, name: 'user' + id })
-        const token = await db.createToken({ user_id: 'u' + id })
+    async function createUserAndSocket(user) {
+        await db.createUser(user)
+        const token = await db.createToken({ user_id: user.id })
         const socket = new SocketHelper()
         await socket.connectAndEvents(process.env.HOST_URL + '/events?token=' + token.id)
         return socket
@@ -72,7 +72,7 @@ describe('events', function() {
         // create group
         await db.createGroup({ id: '1', name: 'destiny'})
         // create user1
-        const s1 = await createUserAndSocket(1)
+        const s1 = await createUserAndSocket({ id: 'u1', name: 'user1' })
         // create the event
         await s1.create({ id: 1, name: 'test event', group_id: '1' })
         // check that we sent a create message
@@ -87,11 +87,11 @@ describe('events', function() {
         // create group
         await db.createGroup({ id: '1', name: 'destiny'})
         // create user1
-        const s1 = await createUserAndSocket(1)
+        const s1 = await createUserAndSocket({ id: 'u1', name: 'user1' })
         // create the event
         await s1.create({ id: 1, name: 'test event', group_id: '1' })
         // create user2
-        const s2 = await createUserAndSocket(2)
+        const s2 = await createUserAndSocket({ id: 'u2', name: 'user2' })
         // join user2
         await s2.join({ event_id: 1, type: 'participant'})
         // check that we sent a join message
@@ -105,11 +105,11 @@ describe('events', function() {
         // create group
         await db.createGroup({ id: '1', name: 'destiny'})
         // create user1
-        const s1 = await createUserAndSocket(1)
+        const s1 = await createUserAndSocket({ id: 'u1', name: 'user1' })
         // create the event
         await s1.create({ id: 1, name: 'test event', group_id: '1' })
         // create user2
-        const s2 = await createUserAndSocket(2)
+        const s2 = await createUserAndSocket({ id: 'u2', name: 'user2' })
         // join user2
         await s2.join({ event_id: 1, type: 'participant'})
         // check that we joined as a participant
@@ -125,11 +125,11 @@ describe('events', function() {
         // create group
         await db.createGroup({ id: '1', name: 'destiny'})
         // create user1
-        const s1 = await createUserAndSocket(1)
+        const s1 = await createUserAndSocket({ id: 'u1', name: 'user1' })
         // create the event
         await s1.create({ id: 1, name: 'test event', group_id: '1' })
         // create user2
-        const s2 = await createUserAndSocket(2)
+        const s2 = await createUserAndSocket({ id: 'u2', name: 'user2' })
         // join user2
         await s2.join({ event_id: 1, type: 'alternative'})
         // check that we joined as an alternative
@@ -145,15 +145,15 @@ describe('events', function() {
         // create group
         await db.createGroup({ id: '1', name: 'destiny'})
         // create user1
-        const s1 = await createUserAndSocket(1)
+        const s1 = await createUserAndSocket({ id: 'u1', name: 'user1' })
         // create the event
         await s1.create({ id: 1, name: 'test event', group_id: '1', max_participants: 2  })
         // create user2
-        const s2 = await createUserAndSocket(2)
+        const s2 = await createUserAndSocket({ id: 'u2', name: 'user2' })
         // join user2
         await s2.join({ event_id: 1, type: 'participant'})
         // create user3
-        const s3 = await createUserAndSocket(3)
+        const s3 = await createUserAndSocket({ id: 'u3', name: 'user3' })
         // join user2
         await s3.join({ event_id: 1, type: 'participant'})
         // check that we joined as an alternative
@@ -200,9 +200,9 @@ describe('events', function() {
         // create group
         await db.createGroup({ id: '1', name: 'destiny'})
 
-        const s1 = await createUserAndSocket(1)
-        const s2 = await createUserAndSocket(2)
-        const s3 = await createUserAndSocket(3)
+        const s1 = await createUserAndSocket({ id: 'u1', name: 'user1' })
+        const s2 = await createUserAndSocket({ id: 'u2', name: 'user2' })
+        const s3 = await createUserAndSocket({ id: 'u3', name: 'user3' })
 
         // create the event and await the update
         const res = await Promise.all([
