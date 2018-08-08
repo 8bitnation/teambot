ff382099385345c702220c4ff676e09bf1431167
Scott Deakin <sdeaks@gmail.com>
2018-08-08 12:45:21 +0100

    remove promise dependency

--- app/static/js/detect.js
+++ app/static/js/detect.js
@@ -3,9 +3,8 @@
 ;(function() {
     // detect essential features used by event.js
     var hasDefineProperty = Boolean(Object.defineProperty)
-    var hasPromise = Boolean(window.Promise)
     // 
-    if(!hasDefineProperty || !hasPromise) {
+    if(!hasDefineProperty) {
         var el = document.getElementById('detect')
         if(el) {
             el.style.display = 'block'
