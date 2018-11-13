8e46ff18bd9f5970947ae5ff1d21059710950968
Scott Deakin <sdeaks@gmail.com>
2018-11-13 08:56:01 +0000

    tweaks to event status message

--- app/discord.js
+++ app/discord.js
@@ -214,7 +214,7 @@ async function isAdmin(user_id) {
 
 async function updateEventMessage(trx, event, message) {
     const guild = client.guilds.get(process.env.DISCORD_GUILD)
-    message.embed.addField('──────────', 'Time shown below is local.  Android will incorrectly show future dates as today')
+    message.embed.addField('─', '*Time shown below is in your timezone.  Android will incorrectly show future dates as today*')
     if(!guild) return logger.error('updateEventMessage: failed to find guild %s', process.env.DISCORD_GUILD)
 
     const channel = guild.channels.get(event.channel_id)
